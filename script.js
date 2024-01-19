8d52edd240ded7f5ce0a38ac5513a7453662f91d
Rafael Assunção <rafaaslat@gmail.com>
2024-01-18 23:12:18 -0300

    alguns projetos adicionados

--- script.js
+++ script.js
@@ -9,23 +9,37 @@ function voltarImagem(imagem, num){
 function preencherProjeto(project){
     const projects = document.querySelector('.projects');
     const newProject = document.createElement('div');
+    let pageLink = "";
+    let appDiv = "";
+    if(project.page){
+        pageLink = `<a href="${project.page}">Visitar página</a>`;
+    }
+    if(project.aplicativo){
+        appDiv = `<div class="language Aplicativo">Aplicativo</div>`
+    }
     newProject.innerHTML = `<div class="item">
     <img src="assets/projetos/projeto${project.number}_1.jpg">
     <div class="details">
-        <h3>${project.name}</h3>
+        <h2>${project.name}</h2>
         <div class="item-info">
-            <div class="language">${project.language}</div>
+            <div class="language ${project.language}">${project.language}</div>
+            ${appDiv}
         </div>
         <p>${project.description}</p>
         <div class="buttons">
-            <a href="${project.git}">Saiba mais</a>
-            <a href="${project.page}">Visitar página</a>
+            <a href="${project.git}" target="_blank">Saiba mais</a>
+            ${pageLink}
         </div>
     </div>
     </div>`
     const img = newProject.querySelector('img');
     img.addEventListener("mouseover", function(){
-        mudarImagem(img, project.number);
+        img.style.opacity = 0; // Define a opacidade para 0
+        setTimeout(() => {
+            // Altera o src após um pequeno intervalo para permitir a transição
+            mudarImagem(img, project.number); // Substitua pelo novo caminho da imagem que deseja exibir
+            img.style.opacity = 1; // Define a opacidade de volta para 1
+        }, 0); // Ajuste o valor conforme necessário para a duração da transição
     });
     img.addEventListener("mouseout", function(){
         voltarImagem(img, project.number);
