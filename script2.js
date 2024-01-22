7a8b5aea3a652d785f114baafaf2c0008b5a99cd
Rafael Assunção <rafaaslat@gmail.com>
2024-01-22 02:02:50 -0300

    meus conhecimentos

--- script2.js
+++ script2.js
@@ -9,20 +9,37 @@ function voltarImagem(imagem, num){
 function preencherProjeto(project){
     const projects = document.querySelector('.main-projects');
     const newProject = document.createElement('div');
+    let appTxt = "";
+    let pageLink = "";
+    if(project.aplicativo){
+        appTxt = `<p class="language Aplicativo">Aplicativo</p>`
+    }
+    if(project.page){
+        pageLink = `<a href="${project.page}">Visitar página</a>`;
+    }
     newProject.innerHTML = `<div class="main-project">
     <img src="assets/projetos/projeto${project.number}_1.jpg" alt="">
     <div class="info">
         <h3>${project.name}</h3>
-        <p class="language ${project.language}">${project.language}</p>
+        <div class="badge">
+            <p class="language ${project.language}">${project.language}</p>
+            ${appTxt}
+        </div>
         <p class="txt">${project.description}</p>
+        <div class="buttons">
+            <a href="${project.git}" target="_blank">Saiba mais</a>
+            ${pageLink}
+        </div>
     </div>
     </div>`
     const img = newProject.querySelector('img');
+    const bts = newProject.querySelector('.buttons');
     newProject.addEventListener("mouseover", function(){
         mudarImagem(img, project.number);
     });
     newProject.addEventListener("mouseout", function(){
         voltarImagem(img, project.number);
+
     });
 
     projects.appendChild(newProject);
